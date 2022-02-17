bf005642266bb99b6dd5bab2a9fe62d88d54d1e3
Vilmarys R <43301644+vilrodriguez@users.noreply.github.com>
2022-02-17 10:56:05 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 import validator from './validator.js';
 
-console.log(validator);
+const formulario = document.querySelector('#formulario-tarjeta');
+//INPUT NUMBER GENERAL VALIDATION --------------------------
+formulario.inputNumero.addEventListener('keyup', (e)=>
+{
+  let valorInput = e.target.value;
+
+  formulario.inputNumero.value = valorInput
+  // Eliminamos espacios en blanco
+  .replace(/\s/g, '')
+  // Eliminar las letras
+  .replace(/\D/g, '')
+  // Espacio cada 4 numeros
+  .replace(/([0-9]{4})/g, '$1 ')
+  // Elimina el ultimo especio
+  .trim();
+})
+// NAME VALIDATION ----------------------------------------------------
+formulario.inputNombre.addEventListener('keyup', (e)=>
+{
+  let valorName = e.target.value;
+  formulario.inputNombre.value = valorName.replace(/\d/g, '').toUpperCase();  //  permite solo números y letras mayúsculas
+})
+// CALL FUNCTIONS WHEN PRESS BOTTON SUBMIT ----------------------------------------------------
+const btnValid = document.querySelector('#validar');
+btnValid.addEventListener("click", (e) =>
+{
+  document.getElementById("fillInputs").innerHTML = "";
+
+  const num = document.getElementById("inputNumero").value;
+  const name = document.getElementById("inputNombre").value;
+  if((num == "") || (name == "")){
+    e.preventDefault();
+    document.getElementById("fillInputs").innerHTML = "Llena el campo que falta";
+  }
+  else{
+
+  const num = document.getElementById("inputNumero").value;
+
+  let num1 = num.replace(/\s/g, ''); // Eliminamos espacios en blanco
+
+  //maskify
+ const maskNumber = validator.maskify(num1);
+
+ //isvalid
+ const isvalidNumber = validator.isValid(num1);
+
+ if(isvalidNumber==true){
+  document.getElementById("numHided").innerHTML = "El número de tarjeta " + maskNumber + " es válido.";
+  document.getElementById("formulario-tarjeta").style.display = 'none';
+  document.getElementById("formulario-tarjeta2").style.display = 'block';
+  document.getElementById("numHided2").innerHTML = "El número de tarjeta " + maskNumber + " es válido.";
+ }
+ else{
+  document.getElementById("numHided").innerHTML = "El número de tarjeta " + maskNumber + " es inválido.";
+  document.getElementById("inputNumero").value = "";
+ }
+}
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
-  // ...
+//*+***************************************IS_VALID**************************************
+  isValid: function(num){
+
+  let nCheck = 0, bEven = false;
+  for(var n = num.length-1; n >= 0; n--){
+    var cDigit = num.charAt(n),
+    nDigit = parseInt(cDigit, 10);
+    if (bEven && (nDigit *= 2) > 9)nDigit -= 9;
+    nCheck +=  nDigit;
+    bEven = !bEven;
+  }
+  return ((nCheck % 10) == 0);
+  },
+//*+******************************************MASKIFY**************************************
+  maskify: function (num)
+{
+  let hideNum = [];
+  for(let i = 0; i < num.length; i++)
+  {
+    if(i < num.length-4)
+    {
+      hideNum.push("#");
+    }
+    else
+    {
+      hideNum.push(num[i]);
+    }
+  }
+  let hideNum1 = hideNum.join("");
+  //let hideNum2 = hideNum1.replace(/([#-9]{4})/g, '$1 '); //Agrupa de 4 en 4
+  return hideNum1;
+}
 };
 
 export default validator;
